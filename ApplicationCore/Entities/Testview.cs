06ca27185c1b9ac7792743879d79fa0eb376700b
xyuc7 <95353188+xyuc7@users.noreply.github.com>
2024-12-12 12:54:21 +0800

    add User Secrets and SSMS scaffolding

---
+++ ApplicationCore/Entities/Testview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Entities;
+
+public partial class Testview
+{
+    public string? CustomerId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public int? ShipVia { get; set; }
+}
